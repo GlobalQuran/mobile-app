6c4e090eeb58a6e3a1ba0f5cdfbe7f966634d503
basit <i@basit.me>
2016-02-21 07:01:23 +0000

    refactor surah detail with proper observables

---
+++ app/GlobalQuran/interface/surahDetail.ts
@@ -0,0 +1,26 @@
+/*
+ arabic_name: "النساء"
+ ayahs: 176
+ english_meaning: "The Women"
+ english_name: "An-Nisaa"
+ no: 4
+ order: 92
+ rukus: 24
+ start: 493
+ type: "Medinan"
+ */
+
+export interface surahDetail {
+
+    arabic_name: string,
+    english_meaning: string,
+    english_name: string,
+
+    ayahs: number,
+    no: number,
+    order: number,
+    rukus: number,
+    start: number,
+    type: string
+
+}
--- app/pages/surah-detail/surah-detail.ts
+++ app/pages/surah-detail/surah-detail.ts
@@ -1,13 +1,6 @@
 import {Page, NavController, NavParams} from 'ionic-framework/ionic';
-
-import {Pipe, PipeTransform} from 'angular2/core';
-
 import {gq} from '../../GlobalQuran/gq';
-
-import 'rxjs/add/operator/map';
-import 'rxjs/operator/delay';
-import 'rxjs/operator/mergeMap';
-import 'rxjs/operator/switchMap';
+import {SettingPage} from "../setting/setting";
 
 
 
@@ -19,22 +12,14 @@ export class SurahDetailPage {
   selectedSurah:any;
   content: any;
 
-  constructor(private nav: NavController, navParams: NavParams, private gq: gq) {
-
+  constructor(private nav: NavController, navParams: NavParams, private gq: gq)
+  {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedSurah = navParams.get('surah');
-  }
 
-  onPageWillEnter()
-  {
-    this.gq.listen('content').subscribe(list => this.setContent(list));
-    this.gq.select(this.selectedSurah.no, 1);
-    this.gq.getContent(this.selectedSurah.no);
-  }
-
-  setContent (list)
-  {
-    this.content = list;
-    console.log(list);
+    this.gq
+        .select(this.selectedSurah.no, 1)
+        .getContent()
+        .subscribe(list => this.content = list);
   }
 }
