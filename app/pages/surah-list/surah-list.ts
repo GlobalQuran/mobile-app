521972cc5582f15c11966240c5729455029ffb16
basit <i@basit.me>
2016-02-21 07:01:07 +0000

    refactor surah list with proper observables

--- app/pages/surah-list/surah-list.ts
+++ app/pages/surah-list/surah-list.ts
@@ -2,27 +2,32 @@ import {Page, NavController, NavParams} from 'ionic-framework/ionic';
 import {gq} from '../../GlobalQuran/gq';
 
 import {SurahDetailPage} from '../surah-detail/surah-detail';
+import {SettingPage} from "../setting/setting";
 
 
 @Page({
-  templateUrl: 'build/pages/surah-list/surah-list.html'
+    templateUrl: 'build/pages/surah-list/surah-list.html'
 })
 export class SurahListPage {
 
-  surahList = [];
+    surahList = [];
 
-  constructor(private nav: NavController, navParams: NavParams, private gq: gq) {}
+    constructor(private nav:NavController, private gq:gq)
+    {
+        this.gq
+            .getSurahList()
+            .subscribe(list => this.surahList.push(list));
+    }
 
-  onPageWillEnter()
-  {
-    this.gq.listen('list.surah').subscribe(list => this.surahList = list);
-    this.gq.getSurahList();
-  }
+    surahTapped(event, surah)
+    {
+        this.nav.push(SurahDetailPage, {
+            surah: surah
+        });
+    }
 
-  surahTapped(event, surah)
-  {
-    this.nav.push(SurahDetailPage, {
-      surah: surah
-    });
-  }
+    openSetting()
+    {
+        this.nav.push(SettingPage);
+    }
 }
